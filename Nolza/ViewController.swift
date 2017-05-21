a45fb1d7478719ac7612cb75318a0339ef0943d4
skrth00 <skrth00@naver.com>
2017-05-21 15:16:45 +0900

    Project 생성

---
+++ Nolza/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nolza
+//
+//  Created by 전한경 on 2017. 5. 21..
+//  Copyright © 2017년 jeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
