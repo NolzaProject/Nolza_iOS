a58383f927d513463e8e2ca1d53da770b6393c3c
skrth00 <skrth00@naver.com>
2017-08-24 21:32:05 +0900

    [Nolza-5] Join us, Sign in 화면 UI 작업 완료

---
+++ Nolza/JoinViewController.swift
@@ -0,0 +1,30 @@
+//
+//  JoinViewController.swift
+//  Nolza
+//
+//  Created by 전한경 on 2017. 8. 24..
+//  Copyright © 2017년 jeon. All rights reserved.
+//
+
+import UIKit
+
+class JoinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
